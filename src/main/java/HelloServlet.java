589fc71385778dde1f0b73d98d2a9b63844aaf30
novitskynick <novitskynick@gmail.com>
2022-05-25 15:31:15 +0300

    all works

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "HelloWorld";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
